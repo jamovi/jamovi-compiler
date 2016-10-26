8d22dacd7be9c5890028b4c679675cfdd9f4dcaa
Jonathon Love <jon@thon.cc>
2016-10-26 16:26:18 +1100

    Incorporated versions into analysis compiler

--- compiler.js
+++ compiler.js
@@ -5,6 +5,11 @@ const path = require('path');
 const fs = require('fs');
 const yaml = require('js-yaml');
 const _ = require('underscore');
+const semver = require('semver');
+
+const reject = function(filePath, message) {
+    throw path.basename(filePath) + ' ' + message;
+}
 
 const compile = function(analysisPath, resultsPath, templPath, outPath) {
 
@@ -19,6 +24,20 @@ const compile = function(analysisPath, resultsPath, templPath, outPath) {
     catch (e) {
         results = null;
     }
+
+    if (typeof analysis.name === 'undefined')
+        reject(analysisPath, 'does not contain an analysis name');
+    if (typeof analysis.title === 'undefined')
+        reject(analysisPath, 'does not contain an analysis title');
+    if (typeof analysis.version === 'undefined' || ! semver.valid(analysis.version))
+        reject(analysisPath, 'does not contain a valid version');
+    if (typeof analysis.api === 'undefined')
+        reject(analysisPath, 'does not specify an API version');
+    if (typeof analysis.api !== 'number')
+        reject(analysisPath, 'specifies a non-numeric API version');
+    if (analysis.api > 1)
+        reject(analysisPath, 'uses a newer API version (requires a newer jamovi-compiler)');
+
     let template = fs.readFileSync(templPath, 'utf-8');
     let compiler = _.template(template);
 
@@ -33,7 +52,7 @@ const compile = function(analysisPath, resultsPath, templPath, outPath) {
     fs.writeFileSync(outPath, content);
 };
 
-const sourcify = function(object, optionName, optionValue) {
+const sourcify = function(object, optionName, optionValue, indent) {
     let str = '';
     if (object === null) {
         str = 'NULL';
@@ -49,23 +68,23 @@ const sourcify = function(object, optionName, optionValue) {
     }
     else if (_.isArray(object)) {
         str = 'list('
-        let sep = '';
+        let sep = '\n' + indent + '    ';
         for (let value of object) {
-            str += sep + sourcify(value, optionName, optionValue);
-            sep = ', '
+            str += sep + sourcify(value, optionName, optionValue, indent + '    ');
+            sep = ',\n' + indent + '    ';
         }
         str += ')';
     }
     else if (_.isObject(object)) {
         if (object.type) {
-            str = optionify(object, optionName, optionValue)
+            str = optionify(object, optionName, optionValue, indent + '    ')
         }
         else {
             str = 'list(';
             let sep = '';
             for (let prop in object) {
                 let value = object[prop];
-                str += sep + prop + '=' + sourcify(value, optionName, optionValue);
+                str += sep + prop + '=' + sourcify(value, optionName, optionValue, indent);
                 sep = ', '
             }
             str += ')';
@@ -74,14 +93,16 @@ const sourcify = function(object, optionName, optionValue) {
     return str;
 }
 
-const optionify = function(option, optionName, optionValue) {
+const optionify = function(option, optionName, optionValue, indent) {
 
     if (option.name)
         optionName = option.name;
     if (typeof optionValue === 'undefined')
         optionValue = option.name;
+    if (typeof indent === 'undefined')
+        indent = '                ';
 
-    let str = 'jmvcore::Option' + option.type + '$new("' + optionName + '", ' + optionValue
+    let str = 'jmvcore::Option' + option.type + '$new(\n' + indent + '"' + optionName + '",\n' + indent + optionValue
 
     for (let prop in option) {
         if (prop === 'type' ||
@@ -90,7 +111,7 @@ const optionify = function(option, optionName, optionValue) {
             prop === 'description')
                 continue;
 
-        str += ', ' + prop + '=' + sourcify(option[prop], optionName, 'NULL');
+        str += ',\n' + indent + prop + '=' + sourcify(option[prop], optionName, 'NULL', indent);
     }
 
     str += ')'
