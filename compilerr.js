810d213ee99ecd9821077ef6f5bfd9bc06c7598a
Jonathon Love <jon@thon.cc>
2016-12-26 21:46:15 +1100

    Dependencies now included in module

--- compilerr.js
+++ compilerr.js
@@ -2,25 +2,37 @@
 'use strict';
 
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const util = require('util');
 const sh = require('child_process').execSync;
 
 const compile = function(srcDir, moduleDir) {
 
     let rDir = path.join(moduleDir, 'R');
+    let buildDir = path.join(srcDir, 'build')
 
     try {
-        fs.statSync(rDir);
+        fs.statSync(buildDir);
     }
     catch (e) {
-        fs.mkdirSync(rDir);
+        fs.mkdirSync(buildDir);
     }
 
-    let basePath = path.join(moduleDir, '..', 'base', 'R');
-    let cmd = util.format('R CMD INSTALL --no-test-load --library="%s" "%s"', rDir, srcDir);
-    console.log(cmd);
-    console.log(sh(cmd, { encoding: 'utf-8', env: { R_LIBS: basePath } } ));
+    let rLibs = buildDir + ':' + path.join(__dirname, 'rlibs');
+
+    try {
+        console.log('Installing dependencies')
+        let cmd;
+        cmd = util.format('R --silent -e "devtools::install_deps(\'%s\', upgrade_dependencies=FALSE, lib=\'%s\')"', srcDir, buildDir);
+        sh(cmd, { stdio: 'inherit', encoding: 'utf-8', env: { R_LIBS: rLibs } } );
+        cmd = util.format('R --silent -e "devtools::install(\'%s\', upgrade_dependencies=FALSE, lib=\'%s\')"', srcDir, buildDir);
+        sh(cmd, { stdio: 'inherit', encoding: 'utf-8', env: { R_LIBS: rLibs } } );
+    }
+    catch (e) {
+        throw 'Could not build module';
+    }
+
+    fs.copySync(buildDir, rDir);
 }
 
 module.exports = compile
