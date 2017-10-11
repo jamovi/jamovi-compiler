dd15db617e3f13621f8e4f78576bdc2e429c7906
Jonathon Love <jon@thon.cc>
2017-10-11 14:59:43 +1100

    Added to error checking

--- compiler.js
+++ compiler.js
@@ -80,6 +80,8 @@ const compile = function(packageName, analysisPath, resultsPath, templPath, outP
         reject(analysisPath, "no 'jas' present");
 
     let jas = analysis.jas.match(/^([0-9]+)\.([0-9]+)$/)
+    if (jas === null)
+        throw 'bad version string: ' + analysis.jas;
     if (parseInt(jas[1]) !== 1 || parseInt(jas[2]) > 1)
         reject(analysisPath, 'requires a newer jamovi-compiler');
 
