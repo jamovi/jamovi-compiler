ca6159b0f2235834652195a0a9ceb2a5bf96bed4
Jonathon Love <jon@thon.cc>
2018-03-27 17:14:51 +1100

    Added compatibility with 0.8.6

--- installer.js
+++ installer.js
@@ -82,7 +82,7 @@ const check = function(jamovi_home) {
 
         if (mas < 0 || maj < 8 || (maj === 8 && min < 1))
             throw 'a newer version of jamovi is required, please update to the newest version';
-        if (mas > 0 || maj > 8 || (maj === 8 && min > 5))
+        if (mas > 0 || maj > 8 || (maj === 8 && min > 6))
             throw 'a newer version of the jamovi-compiler (or jmvtools) is required';
 
         if (match) {
